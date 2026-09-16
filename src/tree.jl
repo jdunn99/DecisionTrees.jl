7fea99cc550727a6d3edb52a9f11e5ad204b7337
Jack Dunn <jmhjdunn123@outlook.com>
2026-09-16 15:22:42 -0400

    Update fit_tree. Added cost complexity parameter

--- src/tree.jl
+++ src/tree.jl
@@ -26,12 +26,27 @@ mutable struct TNode{T}
 end
 
 """ Metrics """
-count_leaves(node::TNode) = node.is_leaf ? 1 : count_leaft(node.left) + count_leaves(node.right)
+count_leaves(node::TNode) = node.is_leaf ? 1 : count_leaves(node.left) + count_leaves(node.right)
 
 tree_error(::ClassificationCriterion, actual::AbstractVector, predicted::AbstractVector) = Float64(sum(actual .!= predicted))
 tree_error(::RegressionCriterion, actual::AbstractVector, predicted::AbstractVector) = sum((actual .- predicted).^2)
 
 subtree_error(node::TNode) = node.is_leaf ? node.error : subtree_error(node.left) + subtree_error(node.right)
+function get_subtree(node::TNode)
+	nodes = TNode[]
+	_get_subtree!(nodes, node)
+	return nodes
+end
+
+# Helper recurse function
+function _get_subtree!(nodes::Vector{TNode}, node::TNode)
+	if !node.is_leaf
+		push!(nodes, node)
+		_get_subtree!(nodes, node.left)
+		_get_subtree!(nodes, node.right)
+	end
+	return nothing
+end
 
 """ Tree Prediction """ 
 # function prediction(::ClassificationCriterion, target_values::AbstractVector)nd
@@ -57,6 +72,53 @@ end
 predict(tree::TNode, observation::NamedTuple) = predict_row(tree, observation)
 
 """ Pruning """
+function print_cp(node::TNode)
+	root_error = node.error
+	generated_alphas = generate_alphas(node)
+
+	t = generated_alphas.alphas ./ root_error
+
+	return t
+end
+
+function prune!(node::TNode)
+	node.is_leaf = true
+	node.left = nothing
+	node.right = nothing
+end
+
+function generate_alphas(tree::TNode)
+	subtrees = [deepcopy(tree)]
+	alphas = [0.0]
+	current = deepcopy(tree)
+
+	while count_leaves(current) > 1
+		best_alpha = Inf
+		weakest_node = nothing
+
+		for node in get_subtree(current)
+			error = node.error
+			sub_error = subtree_error(node)
+			leaves = count_leaves(node)
+
+			alpha = (error - sub_error) / (leaves - 1)
+
+			if alpha < best_alpha
+				best_alpha = alpha
+				weakest_node = node
+			end
+		end
+
+		prune!(weakest_node)
+
+		push!(subtrees, deepcopy(current))
+		push!(alphas, best_alpha)
+	end
+
+	println("We reached the end")
+
+	return (subtrees=subtrees, alphas=alphas)
+end
 
 """ Building Tree """ 
 
@@ -125,20 +187,20 @@ function fit_tree(
 	features::Vector{Symbol},
 	criterion::Criterion,
 	minsplit::Int = 10,
-	maxdepth::Int = 5,
-	currentdepth::Int = 0,	
+	maxdepth::Int = 2,
+	currentdepth::Int = 0,
 )
 	target_values = data[!, target]
 	classes = unique_classes(target_values)
 	pred = prediction(criterion, target_values)
+	current_error = tree_error(criterion, target_values, fill(pred, length(target_values)))
 
-	# TODO: Make a dispatched prediction function to handle criterion.
-	# This only works with classification right now.
-	length(unique(target_values)) == 1 && return TNode(pred, 0.0)
+	length(unique(target_values)) == 1 && return TNode(pred, current_error)
+	currentdepth >= maxdepth && return TNode(pred, current_error)
+	length(target_values) < minsplit && return TNode(pred, current_error)
 
 	split = best_split(data, target_values, features, criterion)
-
-	split.gain == -Inf && return TNode(pred, 0.0)
+	split.gain == -Inf && return TNode(pred, current_error)
 
 	# Split the tree based on the best threshold value
 	best_feature = data[!, split.feature]
@@ -148,8 +210,8 @@ function fit_tree(
 	right_split = @view data[.!threshold_split, :]
 
 	# Recurse on subtrees
-	left = fit_tree(left_split, target, features, criterion)
-	right = fit_tree(right_split, target, features, criterion)
+	left = fit_tree(left_split, target, features, criterion, minsplit, maxdepth, currentdepth + 1)
+	right = fit_tree(right_split, target, features, criterion, minsplit, maxdepth, currentdepth + 1)
 
-	return TNode(pred, 0.0, split.feature, split.threshold, left, right)
+	return TNode(pred, current_error, split.feature, split.threshold, left, right)
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -14,5 +14,9 @@ using Test
 
     tree = DecisionTrees.fit_tree(data.train_data, target_reg, features_reg, DecisionTrees.MSECriterion())
     pred = DecisionTrees.predict(tree, data.test_data)
-    @show pred
+
+    # pruning_result = DecisionTrees.generate_alphas(tree)
+    cp = DecisionTrees.print_cp(tree)
+
+    @show cp
 end
