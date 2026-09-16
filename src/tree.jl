1507cabd9dc04e264579badc443b01a5e7a28754
Jack Dunn <jmhjdunn123@outlook.com>
2026-09-15 21:31:15 -0400

    Started to change tree node structure to avoid recursion when pruning

--- src/tree.jl
+++ src/tree.jl
@@ -1,7 +1,34 @@
+""" Structures """ 
+
 # Node structure for tree
+
+# Works like a Tree class in C
 abstract type Node end
+mutable struct TNode{T} 
+	is_leaf::Bool
+	prediction::T
+	error::Float64 # Used for pruning
+	feature::Symbol
+	threshold::Float64
+	left::Union{TNode{T}, Nothing}
+	right::Union{TNode{T}, Nothing}
+
+	# Leaf
+	TNode(pred::T, err::Float64) where {T} = new{T}(true, pred, err, :none, 0.0, nothing, nothing)
+
+	# Root
+	TNode(pred::T,
+	 	 err::Float64, 
+	 	 feat::Symbol, 
+	 	 thres::Float64, 
+	 	 l::TNode{T}, 
+	 	 r::TNode{T}
+	 ) where {T} = new{T}(false, pred, err, feat, thres, l, r)
+end
+
 struct Leaf{T}<: Node
 	prediction::T
+	error::Float64 # used for pruning
 end
 
 # Defines a split node with feature, threshold, left, right
@@ -10,11 +37,36 @@ struct Root{T} <: Node
 	threshold::T
 	left::Node
 	right::Node
+	error::Float64
 end
 
+""" Metrics """
+count_leaves(node::TNode) = node.is_leaf ? 1 : count_leaft(node.left) + count_leaves(node.right)
+count_leaves(node::Leaf) = 1
+count_leaves(node::Root) = count_leaves(node.left) + count_leaves(node.right)
+count_splits(tree::Node) = count_leaves(tree) - 1 # Don't include the top root
+
+tree_error(::ClassificationCriterion, actual::AbstractVector, predicted::AbstractVector) = Float64(sum(actual .!= predicted))
+tree_error(::RegressionCriterion, actual::AbstractVector, predicted::AbstractVector) = sum((actual .- predicted).^2)
+
+subtree_error(node::TNode) = node.is_leaf ? node.error : subtree_error(node.left) + subtree_error(node.right)
+subtree_error(node::Leaf) = node.error
+subtree_error(node::Root) = subtree_error(node.left) + subtree_error(node.right)
+
+""" Tree Prediction """ 
 # function prediction(::ClassificationCriterion, target_values::AbstractVector)nd
 prediction(::ClassificationCriterion, target_values::AbstractVector) = argmax(unique_classes(target_values))
 prediction(::RegressionCriterion, target_values::AbstractVector) = round(sum(target_values) / length(target_values), digits=4)
+
+function predict_row(node::TNode, row)
+	node.is_leaf && return node.prediction
+
+	if row[node.feature] <= node.threshold
+		return predict_row(node.left, row)
+	else
+		return predict_row(node.right, row)
+	end
+end
 
 # Recursively predict a row in a DataFrame (aka a single observation)
 function predict_row(node::Leaf, _)
@@ -33,12 +85,22 @@ function predict_row(node::Root, row)
 end
 
 # Predict on an entire DataFrame
-function predict(tree::Node, data::AbstractDataFrame)
+function predict(tree::TNode, data::AbstractDataFrame)
 	return [predict_row(tree, row) for row in eachrow(data)]
 end
 
 # May switch NamedTuple to a different data structure.
-predict(tree::Node, observation::NamedTuple) = predict_row(tree, observation)
+predict(tree::TNode, observation::NamedTuple) = predict_row(tree, observation)
+
+""" Pruning """
+# function cost_complexity_pruning(tree::Node)
+# 	# Don't modify the tree
+# 	subtrees = [deepcopy(tree)]
+# 	alphas = [0.0]
+
+# end
+
+""" Building Tree """ 
 
 # Calculate the split that minimizes loss using the provided criterion
 function best_split(
@@ -114,11 +176,11 @@ function fit_tree(
 
 	# TODO: Make a dispatched prediction function to handle criterion.
 	# This only works with classification right now.
-	length(unique(target_values)) == 1 && return Leaf(pred)
+	length(unique(target_values)) == 1 && return TNode(pred, 0.0)
 
 	split = best_split(data, target_values, features, criterion)
 
-	split.gain == -Inf && return Leaf(pred)
+	split.gain == -Inf && return TNode(pred, 0.0)
 
 	# Split the tree based on the best threshold value
 	best_feature = data[!, split.feature]
@@ -131,5 +193,5 @@ function fit_tree(
 	left = fit_tree(left_split, target, features, criterion)
 	right = fit_tree(right_split, target, features, criterion)
 
-	return Root(split.feature, split.threshold, left, right)
+	return TNode(pred, 0.0, split.feature, split.threshold, left, right)
 end
